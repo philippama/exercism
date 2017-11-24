8f61d98e5f88e3c4847af7e65adae13d7f658882
Philippa Main <philippa.main@autotrader.co.uk>
2017-11-24 18:25:22 +0000

    Refactor & discard unwanted accumulator way.

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,17 +1,6 @@
 object SumOfMultiples {
     def sum(factors: Set[Int], limit: Int): Int = {
-        (1 until limit).withFilter(i => hasFactor(factors, i))
-                .map(i => i)
+        (1 until limit).filter(number => factors.exists(factor => number % factor == 0))
                 .sum
-    }
-
-    def sumWithAccumulator(factors: Set[Int], limit: Int): Int = {
-        var accumulator: Int = 0
-        for (i <- 1 until limit if hasFactor(factors, i)) accumulator = accumulator + i
-        accumulator
-    }
-
-    def hasFactor(factors: Set[Int], number: Int): Boolean = {
-        factors.exists(factor => number % factor == 0)
     }
 }
