c2d86e062e84595b0ed3fc9bad5ab6f110a83b20
Philippa Main <philippa.main@autotrader.co.uk>
2017-11-24 18:27:39 +0000

    Added nicer way with use of _.

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,16 @@
 object SumOfMultiples {
     def sum(factors: Set[Int], limit: Int): Int = {
+        sumBetter(factors, limit)
+    }
+
+    def sumMine(factors: Set[Int], limit: Int): Int = {
         (1 until limit).filter(number => factors.exists(factor => number % factor == 0))
+                .sum
+    }
+
+    // From http://exercism.io/rdblyth
+    def sumBetter(factors: Set[Int], limit: Int): Int = {
+        (1 until limit).filter(n => factors.exists(n % _ == 0))
                 .sum
     }
 }
