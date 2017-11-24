298421758e3e46232d3b8652997f738ca11cb864
Philippa Main <philippa.main@autotrader.co.uk>
2017-11-24 07:59:31 +0000

    SumOfMultiples: All tests passed using range with filter then a collection.

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,5 +1,11 @@
 object SumOfMultiples {
     def sum(factors: Set[Int], limit: Int): Int = {
+        (1 until limit).withFilter(i => hasFactor(factors, i))
+                .map(i => i)
+                .sum
+    }
+
+    def sumWithAccumulator(factors: Set[Int], limit: Int): Int = {
         var accumulator: Int = 0
         for (i <- 1 until limit if hasFactor(factors, i)) accumulator = accumulator + i
         accumulator
