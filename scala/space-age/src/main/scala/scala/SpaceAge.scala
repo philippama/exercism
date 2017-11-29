c6133a6c11910874b54d2a8344b45f606cb8a109
Philippa Main <philippa.main@autotrader.co.uk>
2017-11-29 07:45:23 +0000

    SpaceAge: The same but neater.

--- scala/space-age/src/main/scala/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/scala/SpaceAge.scala
@@ -10,37 +10,14 @@ object SpaceAge {
     private val uranusYearSeconds: Double = earthYearSeconds * 84.016846
     private val neptuneYearSeconds: Double = earthYearSeconds * 164.79132
 
-    def onEarth(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, earthYearSeconds)
-    }
-
-    def onMercury(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, mercuryYearSeconds)
-    }
-
-    def onVenus(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, venusYearSeconds)
-    }
-
-    def onMars(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, marsYearSeconds)
-    }
-
-    def onJupiter(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, jupiterYearSeconds)
-    }
-
-    def onSaturn(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, saturnYearSeconds)
-    }
-
-    def onUranus(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, uranusYearSeconds)
-    }
-
-    def onNeptune(ageInSeconds: Double): Double = {
-        toPlanetYears(ageInSeconds, neptuneYearSeconds)
-    }
+    def onEarth(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, earthYearSeconds)
+    def onMercury(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, mercuryYearSeconds)
+    def onVenus(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, venusYearSeconds)
+    def onMars(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, marsYearSeconds)
+    def onJupiter(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, jupiterYearSeconds)
+    def onSaturn(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, saturnYearSeconds)
+    def onUranus(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, uranusYearSeconds)
+    def onNeptune(ageInSeconds: Double): Double = toPlanetYears(ageInSeconds, neptuneYearSeconds)
 
     private def toPlanetYears(seconds: Double, planetYearSeconds: Double): Double = {
         toTwoDecimalPlaces(seconds / planetYearSeconds)
