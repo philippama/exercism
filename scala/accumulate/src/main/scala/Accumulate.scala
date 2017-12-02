1a506be084a1a0f47e8524dbf8cec542ad121fc5
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-02 18:21:53 +0000

    Accumulate: Passes all tests using tail recursion.

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,9 +1,23 @@
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 class Accumulate {
   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+    usingSimpleAccumulator(f, list)
+//    usingTailRecursion(f, list, List[B]())
+  }
+
+  def usingSimpleAccumulator[A, B](f: (A) => B, list : List[A]): List[B] = {
     val accumulator: mutable.ArrayBuffer[B] = mutable.ArrayBuffer()
     list.foreach(accumulator += f(_))
     accumulator.toList
+  }
+
+  @tailrec
+  final def usingTailRecursion[A, B](f: (A) => B, sourceList : List[A], destList : List[B]): List[B] = {
+    if (sourceList.isEmpty) {
+       destList
+    }
+    else usingTailRecursion(f, sourceList.tail, destList :+ f(sourceList.head))
   }
 }
