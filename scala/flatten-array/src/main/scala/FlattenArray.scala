705322d223346c68087003acd7e39844c9f62474
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-04 22:10:47 +0000

    FlattenArray: Added flatten-array with all tests passed.

---
+++ scala/flatten-array/src/main/scala/FlattenArray.scala
@@ -0,0 +1,14 @@
+object FlattenArray {
+
+    def flatten(list: List[Any]): List[Any] = {
+        list
+                .filter(_ != null)
+                .flatMap(element => {
+                    element match {
+                        case l: List[Any] =>
+                            flatten(l)
+                        case _ => List(element)
+                    }
+                })
+    }
+}
