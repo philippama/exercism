b3148031b61a39c4a210cddebe3673c050c81cda
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-08 22:30:45 +0000

    hamming: String doesn't need turning into a List to split it up to zip it.

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -2,8 +2,6 @@ object Hamming {
     def distance(str1: String, str2: String): Option[Int] = {
         Option(str1)
                 .filter(_.length == str2.length)
-                .map(_.toList
-                        .zip(str2.toList)
-                        .count(t => t._1 != t._2))
+                .map(_.zip(str2).count(t => t._1 != t._2))
     }
 }
