7ecfcda12de0af6d4411607dedb255c31e7ce098
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-08 18:59:28 +0000

    Collatz Conjecture: Much cleaner solution from http://exercism.io/submissions/944d09b53fba4d43b358755b3407938c.

--- scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,22 +1,12 @@
 import scala.annotation.tailrec
 
-case class Iteration(stepNumber: Int, value: Int)
-
 object CollatzConjecture {
-
-    def steps(number: Int): Option[Int] = {
-        Option(number)
-            .filter(_ > 0)
-            .map(n => doCollatzIteration(Iteration(0, n)))
-            .map(i => i.stepNumber)
-    }
-
     @tailrec
-    def doCollatzIteration(iteration: Iteration): Iteration = {
-        iteration.value match {
-            case 1 => Iteration(iteration.stepNumber, 1)
-            case value if value % 2 == 0 => doCollatzIteration(Iteration(iteration.stepNumber + 1, value / 2))
-            case value => doCollatzIteration(Iteration(iteration.stepNumber + 1, value * 3 + 1))
+    def steps(number: Int, stepCount: Int = 0): Any =
+        number match {
+            case x if x < 1 => None
+            case 1 => Some(stepCount)
+            case x if x % 2 == 0 => steps(x / 2, stepCount + 1)
+            case x => steps(3 * x + 1, stepCount + 1)
         }
-    }
 }
