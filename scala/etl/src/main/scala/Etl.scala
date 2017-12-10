362a5554a2caed2ab8c6d493d62286a7c3239abf
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-10 17:50:25 +0000

    etl: Added other people's interesting solutions.

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -6,4 +6,15 @@ object Etl {
             letters.map(letter => (letter.toLowerCase(), score))
         })
     }
+
+// After seeing other people's solutions:
+//
+//        legacyMapping.flatMap{case (score, letters) => letters.map(letter => (letter.toLowerCase(), score))}
+//
+// The above works because it is really:
+//        legacyMapping.flatMap(element => element match {
+//            case (score, letters) => letters.map(letter => (letter.toLowerCase(), score))
+//        })
+//
+//        legacyMapping.keys.flatMap(score => legacyMapping(score).map(letter => letter.toLowerCase -> score)).toMap
 }
