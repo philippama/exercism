dac4ca10bdf226151186ebd5073886f35c22e6d1
Philippa Main <philippa.main@autotrader.co.uk>
2017-12-24 07:24:43 +0000

    SecretHandShake: Converted to iterate through a map of binary digits to action.

--- scala/secret-handshake/src/main/scala/SecretHandshake.scala
+++ scala/secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,27 +1,21 @@
 import scala.collection.mutable
 
 object SecretHandshake {
+    val digitActions = Map(
+        0 -> ((handshake: List[String]) => handshake :+ "wink"),
+        1 -> ((handshake: List[String]) => handshake :+ "double blink"),
+        2 -> ((handshake: List[String]) => handshake :+ "close your eyes"),
+        3 -> ((handshake: List[String]) => handshake :+ "jump"),
+        4 -> ((handshake: List[String]) => handshake.reverse)
+    )
+
     def commands(number: Int): List[String] = {
-        Some(List[String]())
-                .map(withBinaryDigit(_, number, 0))
-                .map(withBinaryDigit(_, number, 1))
-                .map(withBinaryDigit(_, number, 2))
-                .map(withBinaryDigit(_, number, 3))
-                .map(withBinaryDigit(_, number, 4))
-                .get
+        digitActions.foldLeft(List[String]()) ((actionList, digitAction) => {
+            if (containsBinaryPower(number, digitAction._1)) digitAction._2(actionList) else actionList
+        })
     }
 
-    def withBinaryDigit(handshake: List[String], number: Int, binaryPower: Int): List[String] = {
-        if ((number & 1 << binaryPower) != 0) {
-            binaryPower match {
-                case 0 => handshake :+ "wink"
-                case 1 => handshake :+ "double blink"
-                case 2 => handshake :+ "close your eyes"
-                case 3 => handshake :+ "jump"
-                case 4 => handshake.reverse
-            }
-        }
-        else handshake
+    private def containsBinaryPower(number: Int, binaryPower: Int) = {
+        (number & (1 << binaryPower)) != 0
     }
-
 }
