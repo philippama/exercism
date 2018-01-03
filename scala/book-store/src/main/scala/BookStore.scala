e65ffed964c3a411a26dcda316947bf4f6c359fd
Philippa Main <philippa.main@autotrader.co.uk>
2018-01-03 17:57:57 +0000

    Book Store: WIP: Applies discounts taking largest batch size possible using recursion.

--- scala/book-store/src/main/scala/BookStore.scala
+++ scala/book-store/src/main/scala/BookStore.scala
@@ -3,14 +3,21 @@ object BookStore {
     val price = 8
 
     var unbatchedBooks = basket.groupBy(book => book).values.toList
-    var bookBatches = List[List[Int]]()
 
-    while (unbatchedBooks.nonEmpty) {
-      bookBatches = bookBatches :+ unbatchedBooks.map(_.head)
-      unbatchedBooks = unbatchedBooks.map(_.tail).filter(_.nonEmpty)
+    batchBooks(unbatchedBooks)
+      .map(_.length)
+      .map(batchPrice(price, _))
+      .sum
+  }
+
+  private def batchBooks(unbatchedBooks: List[List[Int]]): List[List[Int]] = {
+    if (unbatchedBooks.isEmpty) {
+      List[List[Int]]()
     }
-
-    bookBatches.map(_.length).map(batchPrice(price, _)).sum
+    else {
+      val newBatch = unbatchedBooks.map(_.head)
+      newBatch +: batchBooks(unbatchedBooks.map(_.tail).filter(_.nonEmpty))
+    }
   }
 
   private def batchPrice(basicPrice: Int, batchSize: Int) = {
