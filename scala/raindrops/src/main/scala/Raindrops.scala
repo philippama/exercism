f5d8451e59d265ba441007a163b1d8a1b6719592
Philippa Main <philippa.main@autotrader.co.uk>
2018-01-14 07:53:21 +0000

    Raindrops: Nicer solution using filter and reduceLeftOption.

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -2,8 +2,10 @@ object Raindrops {
   def convert(n: Int): String = {
     val factorToSound = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
 
-    Some(factorToSound.collect({ case (factor, sound) if n % factor == 0 => sound }).mkString(""))
-      .filter(_.nonEmpty)
+    factorToSound
+      .filter(mapping => n % mapping._1 == 0)
+      .values
+      .reduceLeftOption(_ + _)
       .getOrElse(n.toString)
   }
 }
