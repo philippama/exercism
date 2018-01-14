b32e4a7fe71aa821731f4182707cbcd854feb025
Philippa Main <philippa.main@autotrader.co.uk>
2018-01-14 06:20:28 +0000

    Raindrops: Added raindrops with all tests passed - Some(...).filter instead of final if...else.

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -1,12 +1,14 @@
 object Raindrops {
   def convert(n: Int): String = {
     val factorToSound = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
-    val sounds = factorToSound.foldLeft("")((sounds, element) => {
-      if (n % element._1 == 0) {
-        sounds + element._2
-      } else sounds
-    })
-    if (sounds.isEmpty) n.toString else sounds
+
+    Some(factorToSound.foldLeft("")((sounds, element) => {
+        if (n % element._1 == 0) {
+          sounds + element._2
+        } else sounds
+      }))
+      .filter(_.nonEmpty)
+      .getOrElse(n.toString)
 
     //    n match {
     //    case num if num % 3 == 0 => "Pling"
