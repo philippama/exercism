40361fcbc2b97e8e776e24e534b605d68a159ab6
Philippa Main <philippa.main@autotrader.co.uk>
2018-01-14 07:23:59 +0000

    Raindrops: Added raindrops with all tests passed - Using collect().

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -2,20 +2,9 @@ object Raindrops {
   def convert(n: Int): String = {
     val factorToSound = Map(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
 
-    Some(factorToSound.foldLeft("")((sounds, element) => {
-        if (n % element._1 == 0) {
-          sounds + element._2
-        } else sounds
-      }))
+    Some(factorToSound.collect({ case (factor, sound) if n % factor == 0 => sound }).mkString(""))
       .filter(_.nonEmpty)
       .getOrElse(n.toString)
-
-    //    n match {
-    //    case num if num % 3 == 0 => "Pling"
-    //    case num if num % 5 == 0 => "Plang"
-    //    case num if num % 7 == 0 => "Plong"
-    //    case num => num.toString
-    //    }
   }
 }
 
