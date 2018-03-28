923ee70bcc266d1022670c27337cdf0ae71d1355
Philippa Main <philippa.main@autotrader.co.uk>
2018-03-28 09:18:05 +0100

    Grains: Use left shift instead of power of 2.

--- scala/grains/src/main/scala/Grains.scala
+++ scala/grains/src/main/scala/Grains.scala
@@ -1,10 +1,10 @@
 object Grains {
-  private val two = BigInt(2)
+  private val one = BigInt(1)
 
   def square(i: Int): Option[BigInt] = {
     Option(i)
       .filter(x => (x >= 1) && (x <= 64))
-      .map(x => two.pow(x - 1))
+      .map(one << _ - 1)
   }
 
   def total: BigInt = (1 to 64).map(square).map(_.get).sum
